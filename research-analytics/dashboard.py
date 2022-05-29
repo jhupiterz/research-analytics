c021870c3f0ad6d9b15c9165bea304e05bc84ebf
Julie Hartz <julie.hartz13@gmail.com>
2022-05-29 18:20:14 +0200

    debug False

--- research-analytics/dashboard.py
+++ research-analytics/dashboard.py
@@ -617,4 +617,4 @@ def displayTapNodeData(data):
 
 # Runs the app ------------------------------------------------------------
 if __name__ == '__main__':
-    app.run_server(debug=True, use_reloader=False)
+    app.run_server(debug=False, use_reloader=False)
